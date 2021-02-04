559c4867076bf7142b5b2d6a668d81d1b824398a
nakai kensuke <nakaikensuke@nakainoMacBook-Air.local>
2021-02-04 21:36:48 +0900

    ログ一覧画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'logs#new'
-  resources :logs
+  resources :logs do
+    collection do
+      get 'return_logs_json'
+    end
+  end
   resources :users
   resources :municipalities
   resources :user_sessions
